db5c4f23c6c72b2233fb608394985b61fff25cb4
Cristian Bazan <johanwebdesigner@gmail.com>
2023-09-12 23:06:12 -0300

    Adding deploy

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import svgr from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
-  base: "https://www.elabrojal.com",
+  base: "https://cristianbazan-dev.github.io/el-abrojal",
 });
