3dcd72202274ed1f274ef4884f547fcb0bc781ee
Cristian Bazan <johanwebdesigner@gmail.com>
2023-09-15 07:31:45 -0300

    Changing categories img

--- src/api/Categories.js
+++ src/api/Categories.js
@@ -3,7 +3,7 @@ export const categories = [
     id: "cad",
     category: "Cadenas",
     categoryImg:
-      "https://res.cloudinary.com/dz6lrvj7u/image/upload/v1694571062/Productos/Categor%C3%ADas/Cadenas_zobqzr_nmtydk.jpg",
+      "https://res.cloudinary.com/dz6lrvj7u/image/upload/v1694571093/Productos/Productos/Cadenas/Derecha/WBG/Cad_-_Der_-_3_yasxwg_ubvbvt.png",
     icon: "https://res.cloudinary.com/dz6lrvj7u/image/upload/v1694571060/Productos/Categor%C3%ADas/icons/chains_ussedm_rad5xi.png",
     haveSubcat: true,
   },
@@ -11,7 +11,7 @@ export const categories = [
     id: "clav",
     category: "Clavos",
     categoryImg:
-      "https://res.cloudinary.com/dhbvri4ni/image/upload/v1691753195/test-el-abrojal/Productos/Categor%C3%ADas/Clavos_vxjfca.png",
+      "https://res.cloudinary.com/dz6lrvj7u/image/upload/v1694773659/Productos/Productos/Clavos/Clav_-_01_-_mod_ivotej.png",
     icon: "https://res.cloudinary.com/dhbvri4ni/image/upload/v1691767752/test-el-abrojal/Productos/Categor%C3%ADas/icons/nails_rrucoz.png",
     haveSubcat: true,
   },
@@ -19,7 +19,7 @@ export const categories = [
     id: "tor",
     category: "Torniquetes",
     categoryImg:
-      "https://res.cloudinary.com/dz6lrvj7u/image/upload/v1694571110/Productos/Categor%C3%ADas/Torniquetes_dtnznl_e2jgaa.png",
+      "https://res.cloudinary.com/dz6lrvj7u/image/upload/v1694571139/Productos/Productos/Torniquetes/Nro6/Tor_-_Nro6_-_2_xo629u_f2wu2k.png",
     icon: "https://res.cloudinary.com/dz6lrvj7u/image/upload/v1694571058/Productos/Categor%C3%ADas/icons/wireStrainers_bhyofh_sofxzw.png",
     haveSubcat: true,
   },
@@ -27,7 +27,7 @@ export const categories = [
     id: "gan",
     category: "Ganchos J",
     categoryImg:
-      "https://res.cloudinary.com/dz6lrvj7u/image/upload/v1694571060/Productos/Categor%C3%ADas/Ganchos_J_ql5liz_bqdlfr.jpg",
+      "https://res.cloudinary.com/dz6lrvj7u/image/upload/v1694773858/Productos/Productos/Ganchos%20J/Gan_-_1_t06nqs_z2tcck.png",
     icon: "https://res.cloudinary.com/dz6lrvj7u/image/upload/v1694571062/Productos/Categor%C3%ADas/icons/hooksJ_g9ybeh_idmxbp.png",
     haveSubcat: true,
   },
@@ -35,7 +35,7 @@ export const categories = [
     id: "arg",
     category: "Argollas",
     categoryImg:
-      "https://res.cloudinary.com/dz6lrvj7u/image/upload/v1694571238/Productos/Categor%C3%ADas/Argollas_cfwvan_xx7sxa.png",
+      "https://res.cloudinary.com/dz6lrvj7u/image/upload/v1694571068/Productos/Productos/Argollas/WBG/Arg_-_3_gwr8zd_z969h0.png",
     icon: "https://res.cloudinary.com/dz6lrvj7u/image/upload/v1694571062/Productos/Categor%C3%ADas/icons/rings_cqnqqp_toxuht.png",
     haveSubcat: true,
   },
@@ -43,7 +43,7 @@ export const categories = [
     id: "mosq",
     category: "Mosquetones",
     categoryImg:
-      "https://res.cloudinary.com/dz6lrvj7u/image/upload/v1694571067/Productos/Categor%C3%ADas/Mosquetones_unbuwh_uj7wkk.jpg",
+      "https://res.cloudinary.com/dz6lrvj7u/image/upload/v1694773446/Productos/Productos/Mosquetones/Mosq_-_01_t3vp1j.png",
     icon: "https://res.cloudinary.com/dz6lrvj7u/image/upload/v1694571058/Productos/Categor%C3%ADas/icons/carabiners_kzb5wi_b7dhqu.png",
     haveSubcat: true,
   },
