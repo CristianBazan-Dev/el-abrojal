3acb08a23627d5f6548bd35a43d53cbb31606cde
Cristian Bazan <johanwebdesigner@gmail.com>
2023-09-18 08:55:32 -0300

    Changing main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,34 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { Router, RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Home from './components/mainPages/Home/Home'
+import Product from './components/utils/product-section/Product'
+
+const router = createBrowserRouter([
+  {
+    path: "/el-abrojal/",
+    element: <App /> ,
+    children: [
+      {
+        path: "/el-abrojal/",
+        element: <Home/>
+      },
+      {
+        path: "/el-abrojal/products",
+        element: <Product/>
+      },
+      {
+        path: "/el-abrojal/about",
+        element: <About/>
+      },
+    ]
+  }
+])
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
