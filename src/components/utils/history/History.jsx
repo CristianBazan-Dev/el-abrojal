cf25cb4d0af2bf1a4f043b5fbdd680fa9715ee37
Cristian Bazan <johanwebdesigner@gmail.com>
2023-09-19 07:24:55 -0300

    Changing routes in main

--- src/components/utils/history/History.jsx
+++ src/components/utils/history/History.jsx
@@ -3,26 +3,19 @@ import CarouselGallery from "../carousel/CarouselGallery";
 import Gallery from "../gallery/Gallery";
 import "./history.css";
 
-import Logo from '../../../assets/logo.png'
+import Logo from "../../../assets/logo.png";
 import WhyUs from "../whyUs/WhyUs";
 
 function History(props) {
   return (
-    <section className="history-section reveal" >
-      <WhyUs/> 
-
-
-    
+    <section className="history-section">
+      <WhyUs />
 
       <div className="sections">
         <div className="second-section">
-          <h2>
-            Entendemos que el mundo está en constante evolución. 
-          </h2>
+          <h2>Entendemos que el mundo está en constante evolución.</h2>
 
-          <h2 className="accent">
-            Nuestra empresa sigue ese curso.
-          </h2>
+          <h2 className="accent">Nuestra empresa sigue ese curso.</h2>
 
           <div className="column">
             <img
@@ -31,26 +24,24 @@ function History(props) {
             />
 
             <div className="text">
-              <h4 style={{textAlign: "left"}}>
+              <h4 style={{ textAlign: "left" }}>
                 A lo largo de los años, nuestra empresa ha sido un faro de
                 calidad, compromiso y progreso desde nuestros primeros pasos.
               </h4>
-              <br/>
+              <br />
               <p>
                 Nuestra especialización en la fabricación de cadenas es la
                 columna vertebral de nuestro éxito. Con el pasar del tiempo,
                 hemos perfeccionado nuestros procesos y hemos invertido en
                 tecnología de vanguardia para ofrecer cadenas que cumplen con
-                los más altos estándares de calidad y durabilidad. 
-               
+                los más altos estándares de calidad y durabilidad.
               </p>
-
             </div>
           </div>
         </div>
 
         <div className="first-section">
-        {/* <div className="titles">
+          {/* <div className="titles">
           <h1>Nuestra historia</h1>
           <div className="subtitle">
             <p>
@@ -60,17 +51,13 @@ function History(props) {
           </div>
         </div> */}
 
-        <div className="background"></div>
-      </div>
-
-
+          <div className="background"></div>
+        </div>
 
         {/* <div className="gallery-section">
           <CarouselGallery />
           <Gallery />
         </div> */}
-
-      
       </div>
     </section>
   );
