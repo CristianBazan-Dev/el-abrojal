224382d99569c91ef0a497894c4267cdd795a506
Cristian Bazan <johanwebdesigner@gmail.com>
2023-09-20 14:07:28 -0300

    Fixing products issue

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import Header from '../components/header/Header';
 import Contact from '../components/contact/Contact';
 import Footer from '../components/footer/Footer';
 import Modal from '../components/utils/modal/Modal';
 
 import ProductsSection from '../components/mainPages/Products/Products'
+import { GlobalState } from '../GlobalState';
+
+
+
+
 function Products(props) {
+    const state = useContext(GlobalState)
+    const [productSelected, setProductSelected] = state.categories.productSelected; 
+    
+    const [categorySelected, setCategorySelected] = state.categories.categorySelected; 
+    
+
+
+    useEffect(() => {
+        setProductSelected("");
+        setCategorySelected("")
+    }, [])
     return (
         <article className="app">
         <Header />
