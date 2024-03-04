9895f8444abe7e3eb33f2c8604584426a5d6a467
Cristian Bazan <johanwebdesigner@gmail.com>
2024-03-03 23:45:48 -0300

    Adding email service

--- src/components/mainPages/Contact/Form/ContactForm.jsx
+++ src/components/mainPages/Contact/Form/ContactForm.jsx
@@ -1,15 +1,46 @@
-import React from "react";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 import "./contactForm.css";
+import toast, { Toaster } from "react-hot-toast";
 
 function ContactForm(props) {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        import.meta.env.VITE_EMAIL_SERVICE_ID,
+        import.meta.env.VITE_EMAIL_TEMPLATE_ID,
+        form.current,
+        import.meta.env.VITE_EMAIL_USER_ID
+      )
+      .then(
+        (result) => {
+          if ((result.status = 200)) {
+            toast.success("E-mail enviado. ¡Gracias por contactarse!");
+          }
+        },
+        (error) => {
+          toast.error(
+            "Hubo un error al enviar el e-mail. Espere un momento o contactese mediante teléfono. ¡Gracias por su paciencia!"
+          );
+          console.log(error.text);
+        }
+      );
+  };
+
   return (
     <div className="form-section">
-      <form action="" className="contact-form">
-        <input type="text" placeholder="Nombre completo" />
-        <input type="text" placeholder="Mail de contacto" />
-        <input type="text" placeholder="Asunto" />
+      <Toaster position="top-center" reverseOrder={false} />
+
+      <form action="" className="contact-form" ref={form} onSubmit={sendEmail}>
+        <input type="text" placeholder="Nombre completo" name="user_name" />
+        <input type="text" placeholder="Mail de contacto" name="user_email" />
+        <input type="text" placeholder="Asunto" name="user_subject" />
         <textarea
-          name=""
+          name="message"
           id=""
           cols="30"
           rows="10"
