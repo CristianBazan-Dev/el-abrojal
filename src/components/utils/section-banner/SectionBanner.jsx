265481a6fe3d94c380a1a470699e978b5d6050b1
Cristian Bazan <johanwebdesigner@gmail.com>
2024-05-02 22:42:43 -0300

    Adding changes

--- src/components/utils/section-banner/SectionBanner.jsx
+++ src/components/utils/section-banner/SectionBanner.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './sectionBanner.css'
 
 function SectionBanner({title, img}) {
+    console.log(title)
     return (
         <section className='sections-banner' style={{background: `url(${img})`}}>
             <h2>{title}</h2>
